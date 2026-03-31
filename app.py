0afb3eb8bf693e57af2fc2a4d5052a4abfae9a90
Cole Campbell <ccampbell211@my.bcit.ca>
2026-03-31 14:39:44 -0700

    Revert "added production deployment config and affiliate links"
    This reverts commit 390766e1036359c6b0d0709b86130703eb590b6d.

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 import dash
 import dash_bootstrap_components as dbc
 from layout import app_layout
@@ -6,7 +5,6 @@ from callbacks import register_callbacks
 import data
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
-server = app.server  # Expose Flask server for gunicorn
 
 data.setup_cache()
 app.layout = app_layout
@@ -14,5 +12,4 @@ register_callbacks(app)
 
 if __name__ == '__main__':
     data.clear_old_cache()
-    port = int(os.environ.get('PORT', 8050))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=8050)
